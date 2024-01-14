e0f55c77de1b5024395adf51eaa2215e36ef2e72
Royce-rz <110698169+Royce-rz@users.noreply.github.com>
2024-01-14 22:36:24 +0000

    main.py

--- AMLS_23-24_SN23124460/main.py
+++ AMLS_23-24_SN23124460/main.py
@@ -1,26 +1,26 @@
-# from A import Logistic_Regression
+from A import Logistic_Regression
 from A import SVM
 from A import DecisionTree
-# from A import Data
-# from B import Logistic_Regression_B
-
+from A import Data
+from B import Logistic_Regression_B
+from B import CNN
 
 if __name__ == "__main__":
-    # print('The labels and images in Task A are :')
-    # Data.imageA()
+    print('The labels and images in Task A are :')
+    Data.imageA()
     
-    # print('The Labels and images in Task B are ')
-    # Data.imageB()
-    # print('-----------------------------')
-    # print('##########################################')
+    print('The Labels and images in Task B are ')
+    Data.imageB()
+    print('-----------------------------')
+    print('##########################################')
    
     
     
     
-    # print('Task A by using Logistic Regresion')
-    # Logistic_Regression.LRans()
-    # print('-----------------------------')
-    # print('##########################################')
+    print('Task A by using Logistic Regresion')
+    Logistic_Regression.LRans()
+    print('-----------------------------')
+    print('##########################################')
 
 
 
@@ -35,5 +35,10 @@ if __name__ == "__main__":
     print('-----------------------------')
     print('##########################################')
     
-    # print('Task B by using Logistic Regresion')
-    # Logistic_Regression_B.LRtaskBans()
+    print('Task B by using Logistic Regresion')
+    Logistic_Regression_B.LRtaskBans()
+    print('-----------------------------')
+    print('##########################################')
+    
+    print('CNN in Task B')
+    CNN.CNNans()
