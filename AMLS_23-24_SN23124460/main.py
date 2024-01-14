6ee5d7eb06b5618b9ee766c3c492f6d0674fc243
Royce-rz <110698169+Royce-rz@users.noreply.github.com>
2024-01-14 22:35:15 +0000

    Add files via upload

---
+++ AMLS_23-24_SN23124460/main.py
@@ -0,0 +1,39 @@
+# from A import Logistic_Regression
+from A import SVM
+from A import DecisionTree
+# from A import Data
+# from B import Logistic_Regression_B
+
+
+if __name__ == "__main__":
+    # print('The labels and images in Task A are :')
+    # Data.imageA()
+    
+    # print('The Labels and images in Task B are ')
+    # Data.imageB()
+    # print('-----------------------------')
+    # print('##########################################')
+   
+    
+    
+    
+    # print('Task A by using Logistic Regresion')
+    # Logistic_Regression.LRans()
+    # print('-----------------------------')
+    # print('##########################################')
+
+
+
+    print('Task A by using SVM')
+    SVM.svmans()
+    print('-----------------------------')
+    print('##########################################')
+
+
+    print('Task A by using Decision Tree')
+    DecisionTree.DecisionTreeans()
+    print('-----------------------------')
+    print('##########################################')
+    
+    # print('Task B by using Logistic Regresion')
+    # Logistic_Regression_B.LRtaskBans()
